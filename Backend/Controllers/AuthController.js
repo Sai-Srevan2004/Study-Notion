9d30ec444cc3925ef4f1cb635085edc74db16762
Sai-Srevan2004 <22bd1a057l@gmail.com>
2024-10-17 23:52:52 +0530

    nodemailer added

---
+++ Backend/Controllers/AuthController.js
@@ -0,0 +1,4 @@
+//sign up
+//login
+//otp
+//change password
--- Backend/Models/OtpModel.js
+++ Backend/Models/OtpModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mailSender = require('../Utils/mailSender')
 
 const OtpSchema = new mongoose.Schema({
     email:{
@@ -16,5 +17,24 @@ const OtpSchema = new mongoose.Schema({
     }
 })
 
+
+//a function -> to send emails
+
+async function sendVerificationEmail(email,otp)
+{
+    try {
+        const mailResponse=await mailSender(email,"Email verification from StudyNotion",otp)
+        console.log("Email sent Successfully!",mailResponse)
+    } catch (error) {
+        console.log("Error while sending email",error)
+        throw error
+    }
+}
+
+
+OtpSchema.pre("save",async function(next){
+   await sendVerificationEmail(this.email,this.otp)
+   next()
+})
 
 module.exports = mongoose.model("OtpModel", OtpSchema)
