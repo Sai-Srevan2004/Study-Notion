d6fa86dd8019c7b68f10c9ab8cf33f3ed4813a85
Sai-Srevan2004 <22bd1a057l@gmail.com>
2024-11-21 19:23:24 +0530

    had to delte complete frontned folder due to some bug issues

--- Frontend/src/Components/core/HomePage/HighlightText.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './HomePageComponent.css';
-
-const HighlightText = ({ text }) => {
-  return <span className="highlight-text">{text}</span>;
-};
-
-export default HighlightText;
